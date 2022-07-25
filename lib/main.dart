79b7773c07f0996c35d98995149f8af826fc0b80
ezeoketochi <tochioluebube@gmail.com>
2022-07-25 23:25:37 +0100

    commiting

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,10 +8,55 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: SafeArea(
-          child: Container(),
+        appBar: AppBar(
+          title: Text("Xylophone App"),
+          centerTitle: true,
         ),
+        // backgroundColor: Colors.teal,
+        body: BodyPage(),
+        // backgroundColor: Colors.white,
       ),
     );
   }
+}
+
+class BodyPage extends StatefulWidget {
+  const BodyPage({Key key}) : super(key: key);
+
+  @override
+  State<BodyPage> createState() => _BodyPageState();
+}
+
+class _BodyPageState extends State<BodyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          MusicNote(Colors.red, 1),
+          MusicNote(Colors.blue, 2),
+          MusicNote(Colors.green, 3),
+          MusicNote(Colors.yellow, 4),
+          MusicNote(Colors.grey, 5),
+          MusicNote(Colors.teal, 6),
+          MusicNote(Colors.green, 7),
+        ],
+      ),
+    );
+  }
+}
+
+Widget MusicNote(Color color, int number) {
+  return Expanded(
+    child: TextButton(
+      child: Container(
+        color: color,
+      ),
+      onPressed: () {
+        final player = AudioCache();
+        player.play("note$number.wav");
+      },
+    ),
+  );
 }
